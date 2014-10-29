ba3f01ffc664cc2e7840994441140db0b11b97b1
Paul Stamy <pstamy@cropquest.com>
2014-10-29 12:17:08 -0500

    Finished chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -6,6 +8,9 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
