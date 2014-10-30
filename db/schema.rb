e01f89c129e0dac28f6ccaeeb9400e4a780d464e
Paul Stamy <pstamy@cropquest.com>
2014-10-30 09:34:18 -0500

    "Finish Chapter 9- user edit, update, index and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029142026) do
+ActiveRecord::Schema.define(version: 20141030141326) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141029142026) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,15 +4,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
   	get signup_path
-	assert_no_difference 'User.count' do
-	  post users_path, user: { name:  "",
-	                           email: "user@invalid",
-	                           password:              "foo",
-	                           password_confirmation: "bar" }
-	end
-	assert_template 'users/new'
-	#assert_select 'div#<CSS id for error explanation>'
-	#assert_select 'div.<CSS class for field with error>'
+	  assert_no_difference 'User.count' do
+  	  post users_path, user: { name:  "",
+  	                           email: "user@invalid",
+  	                           password:              "foo",
+  	                           password_confirmation: "bar" }
+  	end
+  	assert_template 'users/new'
+  	#assert_select 'div#<CSS id for error explanation>'
+  	#assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
@@ -27,6 +27,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: password }
     end
     assert_template 'users/show'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
     assert is_logged_in?
   end
 
